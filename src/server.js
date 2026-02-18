d3507ecaff7c8082056e2156e8cf1d5144f5e69a
HOG <hogseruj1@gmail.com>
2026-02-18 20:16:43 +0200

    Init project.

---
+++ src/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { errors } from 'celebrate';
+// import { connectMongoDB } from './db/connectMongoDB.js';
+// Middlewares
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+// Routers
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(logger);
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// Routes
+
+// Middleware 404
+app.use(notFoundHandler);
+
+// Celebrate errors.
+app.use(errors());
+
+// Middleware Errors.
+app.use(errorHandler);
+
+// Connect Mongo
+// await connectMongoDB();
+
+// Launch Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
