d2b3d7896dd32e926d6f30bb250faca0bfb23dcf
Natalia <onishenko2@gmail.com>
2026-02-20 14:16:42 +0100

    Add private endpoint GET /stories/my with pagination

--- src/routes/storiesRoutes.js
+++ src/routes/storiesRoutes.js
@@ -2,9 +2,12 @@ import { Router } from 'express';
 import { celebrate } from 'celebrate';
 import { getAllStories } from '../controllers/storiesController.js';
 import { getAllStoriesSchema } from '../validations/storiesValidation.js';
+import { authenticate } from '../middleware/authenticate.js';
+import { getOwnStories } from '../controllers/storiesController.js';
 
 const router = Router();
 
 router.get('/stories', celebrate(getAllStoriesSchema), getAllStories);
+router.get('/stories/my', authenticate, getOwnStories);
 
 export default router;
