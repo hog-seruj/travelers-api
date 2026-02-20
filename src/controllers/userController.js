d18bb735e873b87e6e63db29d4b191cd06515dec
Olha Loz <olyakarabyn@gmail.com>
2026-02-20 05:18:28 +0200

    get users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,28 @@ import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
+export const getUsers = async (req, res) => {
+  const { page = 1, perPage = 8 } = req.query;
+  const skip = (page - 1) * perPage;
+
+  const usersQuery = User.find();
+
+  const [totalItems, users] = await Promise.all([
+    usersQuery.clone().countDocuments(),
+    usersQuery.skip(skip).limit(perPage).sort({ articlesAmount: 'desc' }),
+  ]);
+
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalItems,
+    totalPages,
+    users,
+  });
+};
+
 export const updateUserAvatar = async (req, res) => {
   if (!req.file) {
     throw createHttpError(400, 'No file');
