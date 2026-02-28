cd8aa5fdbef2d6fa57e54b2f90b9d88d3c3340f6
Iryna Yermak <1991irenka@gmail.com>
2026-02-28 10:59:11 +0000

    added controllerr getUsers with non-static pagination

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,26 +3,71 @@ import { User } from '../models/user.js';
 import { Stories } from '../models/stories.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
+// get users classic pagination with page and perPage
+// export const getUsers = async (req, res) => {
+//   const page = Number(req.query.page) || 1;
+//   const perPage = Number(req.query.perPage) || 8;
+//   // const { page = 1, perPage = 8 } = req.query;
+//   const skip = (page - 1) * perPage;
+
+//   const usersQuery = User.find();
+
+//   const [totalItems, users] = await Promise.all([
+//     usersQuery.clone().countDocuments(),
+//     usersQuery.skip(skip).limit(perPage).sort({ articlesAmount: 'desc' }),
+//   ]);
+
+//   const totalPages = Math.ceil(totalItems / perPage);
+
+//   res.status(200).json({
+//     page,
+//     perPage,
+//     totalItems,
+//     totalPages,
+//     users,
+//   });
+// };
+
+// get users pagination with another next page for page 2 and beyond
 export const getUsers = async (req, res) => {
-  const { page = 1, perPage = 8 } = req.query;
-  const skip = (page - 1) * perPage;
+  try {
+    let { page, perPage, nextPerPage } = req.query;
 
-  const usersQuery = User.find();
+    page = Number(page) || 1;
+    perPage = Number(perPage) || 12; // ліміт для сторінки №1
+    nextPerPage = nextPerPage != null ? Number(nextPerPage) : perPage;
 
-  const [totalItems, users] = await Promise.all([
-    usersQuery.clone().countDocuments(),
-    usersQuery.skip(skip).limit(perPage).sort({ articlesAmount: 'desc' }),
-  ]);
+    //  count skip та limit
+    const isFirstPage = page === 1;
+    const limit = isFirstPage ? perPage : nextPerPage;
+    const skip = isFirstPage ? 0 : perPage + (page - 2) * nextPerPage;
 
-  const totalPages = Math.ceil(totalItems / perPage);
+    const usersQuery = User.find();
 
-  res.status(200).json({
-    page,
-    perPage,
-    totalItems,
-    totalPages,
-    users,
-  });
+    const [totalItems, users] = await Promise.all([
+      usersQuery.clone().countDocuments(),
+      usersQuery.skip(skip).limit(limit).sort({
+        articlesAmount: 'desc',
+      }),
+    ]);
+
+    //  count totalPages: 1‑st page and perPage, other from nextPerPage
+    const remaining = Math.max(totalItems - perPage, 0);
+    const extraPages = remaining > 0 ? Math.ceil(remaining / nextPerPage) : 0;
+    const totalPages = 1 + extraPages;
+
+    res.status(200).json({
+      page,
+      perPage,
+      nextPerPage,
+      totalItems,
+      totalPages,
+      users,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Failed to get users' });
+  }
 };
 
 // PATCH /users/me/avatar
